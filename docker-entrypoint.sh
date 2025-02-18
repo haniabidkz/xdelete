1dd9b399db0a4b2ad38b7fca824e98681f8f506a
Hasnain Abid Khanzada <haniabidkz@gmail.com>
2025-02-18 23:11:45 +0000

    gf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Explicitly set the PHP binary and Composer binary paths:
-export PHP_BINARY=/usr/local/sbin/php
+export PHP_BINARY=/usr/local/bin/php
 export COMPOSER_BINARY=/usr/bin/composer
 
 # Execute the main command (php-fpm) with any arguments passed to this script
